106551f16fcca925993812b144a7fe657d38d3f2
Eyitope Adelowo <topeboy2009@gmail.com>
2018-04-28 04:45:29 +0100

    Update sim800l.h

--- sim800l.h
+++ sim800l.h
@@ -4,8 +4,8 @@
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 
-#define SIM800_TX_PIN           10
-#define SIM800_RX_PIN           9
+#define SIM800_TX_PIN 10
+#define SIM800_RX_PIN 9
 
 #define TIMEOUT 2000
 #define TRIALS 5
@@ -19,9 +19,8 @@ class NETWORK
 		}
 		int initModule(int trials);
 		int checkResponse(const char *resp, int timeout);
-		int sendCmdAndWaitForResponse(const char *cmd, const char *a_part_of_expected_response, int timeout, int trials=TRIALS);
+		int sendCmdAndWaitForResponse(const char *cmd, const char *a_part_of_expected_response, int timeout, int trials);
 		int initAndSendSMS(const char *message, const char *phone_number);
-		//void showIpAdress(void);
 		void emptyBuffer(char *buff);
 		//int connect1(const char *apn, const char *username, const char *password, int port=80, const char *website);
 		int sendRequest(void);
@@ -39,4 +38,3 @@ class NETWORK
 
 		
 		
-
