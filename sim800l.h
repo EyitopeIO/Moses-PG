559b2929314b4fc4e5f0469849ee7122d5e1dbff
Eyitope Adelowo <topeboy2009@gmail.com>
2018-05-13 03:11:51 +0100

    Update sim800l.h

--- sim800l.h
+++ sim800l.h
@@ -7,27 +7,37 @@
 #define SIM800_TX_PIN 10
 #define SIM800_RX_PIN 9
 
-#define TIMEOUT 2000
+#define TIMEOUT 90000
 #define TRIALS 5
+
+#define SEE(X) Serial.println(X)
+
+#define USE_RESPONSE true
+#define IGNORE_RESPONSE false
+#define RESTART true
+#define CLOSE_ONLY false
 
 class NETWORK
 {
 	public:
-		
-		NETWORK(int baudRate = 9600):serialSIM800(SIM800_TX_PIN,SIM800_RX_PIN){
+
+		NETWORK(int baudRate = 9600):serialSIM800(SIM800_RX_PIN,SIM800_TX_PIN){
 			serialSIM800.begin(baudRate);
 		}
-		int initModule(int trials);
-		int checkResponse(const char *resp, int timeout);
-		int sendCmdAndWaitForResponse(const char *cmd, const char *a_part_of_expected_response, int timeout, int trials);
+		int initModule(unsigned char trials);
+		int checkResponse(const char *resp, unsigned long timeout, bool flag);
+		int sendCmdAndWaitForResponse(const char *cmd, const char *a_part_of_expected_response, unsigned long timeout, unsigned char trials, bool flag);
 		int initAndSendSMS(const char *message, const char *phone_number);
 		void emptyBuffer(char *buff);
 		//int connect1(const char *apn, const char *username, const char *password, int port=80, const char *website);
-		int sendRequest(void);
-		int setupInternet(const char *apn, const char *request);
+		//int sendRequest(void);
+		int sendRequest2(const char *request);
+		int setupInternet2(const char *apn, const char *username, const char *password, int port, const char *website);
+		//int setupInternet(const char *apn, const char *request);
 		int waitForServerResponse(void);
 		char readResponse(void);
-		
+		//void closeNetwork(void);
+		void closeNetwork2(bool flag);
 		
 		SoftwareSerial serialSIM800;
 		
